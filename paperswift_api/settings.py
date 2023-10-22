3f0fab21c47515da40307c78660089394d26f2a7
chandansgowda <chandansuresh007@gmail.com>
2023-10-22 21:34:32 +0530

    feat: setup dj-rest-auth for authentication

--- paperswift_api/settings.py
+++ paperswift_api/settings.py
@@ -37,9 +37,18 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # DRF
     'rest_framework',
+    'rest_framework.authtoken',
+    # Swagger Docs
     'drf_spectacular',
+    # Django Allauth
+    'allauth',
+    'allauth.account',
+    'dj_rest_auth'
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -49,6 +58,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # Django Allauth
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "paperswift_api.urls"
--- paperswift_api/urls.py
+++ paperswift_api/urls.py
@@ -20,10 +20,11 @@ from drf_spectacular.views import (
 )
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'),
          name='api-docs',)
