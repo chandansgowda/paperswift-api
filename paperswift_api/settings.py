bdea73517c411c8b178a90f000521a243a76ffe1
chandansgowda <chandansuresh007@gmail.com>
2023-11-01 12:06:50 +0530

    deploy: updated

--- paperswift_api/settings.py
+++ paperswift_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-dyuqiy+@yi2%g+)ccujcf-uqnxs_(9uwgs*!6!*ht34o1!17sq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['paperswiftsjcetest.pythonanywhere.com']
+ALLOWED_HOSTS = ['paperswiftsjcetest.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -129,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
