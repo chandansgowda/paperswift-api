970200e209542456048e3f424cbb32a0f2d1d9f7
chandansgowda <chandansuresh007@gmail.com>
2023-11-10 20:48:34 +0530

    feat: course and paper setter models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class PaperSetter(models.Model):
+    """
+    Paper Setter Details
+    """
+    psid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, null=False)
+    email = models.EmailField(unique=True, null=False)
+
+class Course(models.Model):
+    """
+    Course Details
+    """
+    code = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, null=False)
+    scheme = models.IntegerField()
+
