d33d3ced73ffe721d357ac36c16fe4e3e1a46fbc
chandansgowda <chandansuresh007@gmail.com>
2023-12-05 20:13:23 +0530

    fix: time zone and cors

--- paperswift_api/settings.py
+++ paperswift_api/settings.py
@@ -132,7 +132,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Kolkata"
 
 USE_I18N = True
 
@@ -153,3 +153,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+
+CORS_ALLOW_ALL_ORIGINS = True
