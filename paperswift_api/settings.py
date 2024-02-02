2a89b942b1cdd171f7e8305071b376f893ec12a6
chandansgowda <chandansuresh007@gmail.com>
2024-02-02 17:43:06 +0530

    fix: missing auth class

--- paperswift_api/settings.py
+++ paperswift_api/settings.py
@@ -152,6 +152,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 
