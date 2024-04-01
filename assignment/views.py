e14a7b5f01b9496d7fdf4ac68caca739bd7cab68
chandansgowda <chandansuresh007@gmail.com>
2024-04-01 23:02:34 +0530

    feat: generate qp upload link

--- assignment/views.py
+++ assignment/views.py
@@ -88,9 +88,11 @@ def set_paper_setter_decision(request):
             assignment.status = "Invite Rejected"
         assignment.save()
 
+        upload_link = f"http://127.0.0.1:49310/#/upload-qp?exam_id={exam_id}&course_code={course_code}&course_name={course.name}&sem={exam.sem}&tracking_token={assignment.tracking_token}"
+
         if assignment.status == "In Progress":
             send_email(assignment.paper_setter.user.email, subject="Question Paper Details", htmlContent=get_qp_details_html(
-                syllabus_copy_link=course.syllabus_doc_url, upload_link="#", deadline=exam.paper_submission_deadline, course_code=course.code, course_name=course.name, branch=course.department, semester=exam.sem, name=assignment.paper_setter.name))
+                syllabus_copy_link=course.syllabus_doc_url, upload_link=upload_link, deadline=exam.paper_submission_deadline, course_code=course.code, course_name=course.name, branch=course.department, semester=exam.sem, name=assignment.paper_setter.name))
 
         return HttpResponse(f"Thanks for your response. Status Updated - {assignment.status}")
     except Exception as e:
